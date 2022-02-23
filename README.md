91a01daef72d9dc2c7f554cf7507962692fc072c
unknown <b_lee12@hotmail.co.uk>
2022-02-23 12:07:10 +0000

    done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # short_url
+
+## Installation 
+- git clone
+- `pipenv install` all the dependencies 
+- `pipenv shell`
+- `python app.py`
+
+## Usage
+- Enter the full url
+- Generates a shortened url which you can click on or paste into the end of the website url
+- Have fun :)
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,23 +10,35 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+urls = {}
+
 def short_url_gen():
     length = 6
-    short_code = "".join(random.choices(string.ascii_letters, k=length))
-    return short_code
+    while True: 
+        short_code = "".join(random.choices(string.ascii_letters, k=length))
+        if short_code not in urls:
+            return short_code
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         input_url = request.form['input_url']
         result = short_url_gen()
+        for key, value in urls.items():
+            if input_url == value:
+                return render_template('index.html', result=key) 
+        urls[result] = input_url
         return render_template('index.html', result=result)
     else:
         return render_template('index.html')
 
 @app.route('/<short_code>')
-def redirect():
-    SELECT long_url FROM URL WHERE short_url = short_code
+def redirect_url(short_code):
+    if short_code in urls:
+        return redirect(f'{urls[short_code]}')
+    else:
+        return render_template('index.html', result="No result found")
 
 if __name__ == "__main__":
     app.run(debug=True)
